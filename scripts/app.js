13a955455f364bfbf76168ce1cb4ffa837a73278
LaurenSwartz <swa21010@byui.edu>
2023-01-13 15:16:42 +0200

    index page

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+const h1 = document.querySelector('h1');
+const copyright =document.querySelector('#copyright');
+
+h1.textContent =`Lauren Swartz`;
+
+copyright.textContent =`©️ ${new Date().getFullYear()}`;
+let quantity = document.querySelector()
