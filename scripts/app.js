9b7c84cd1b42a1eac0da1041e4dbf03011dbc339
LaurenSwartz <swa21010@byui.edu>
2023-01-17 21:09:20 +0200

    changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,40 @@
-const h1 = document.querySelector('h1');
-const copyright =document.querySelector('#copyright');
+let dayname =[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    
+];
+let month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
 
-h1.textContent =`Lauren Swartz`;
+let d = new Date();
+let dayName= dayname[d.getDay()];
+let monthName = month[d.getMonth()];
+let fulldate = dayName + ", " + monthName + ", " + d.getDate() + ", " + d.getFullYear();
 
-copyright.textContent =`©️ ${new Date().getFullYear()}`;
-let quantity = document.querySelector()
+document.getElementById("currentdate").textContent= fulldate;
+
+const copyright = document.querySelector('#copyright');
+copyright. textContent = `©️ ${new Date().getFullYear()}`;
+
+
+
+
+
+
