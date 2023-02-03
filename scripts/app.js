083fa9ba74fec82dc12045f3658ec6d643a8e734
LaurenSwartz <swa21010@byui.edu>
2023-02-03 08:37:38 +0200

    book of mormon lesson

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,6 @@
-let dayname =[
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-    
-];
-let month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-];
-
-let d = new Date();
-let dayName= dayname[d.getDay()];
-let monthName = month[d.getMonth()];
-let fulldate = dayName + ", " + monthName + ", " + d.getDate() + ", " + d.getFullYear();
-
-document.getElementById("currentdate").textContent= fulldate;
-
-const copyright = document.querySelector('#copyright');
-copyright. textContent = `©️ ${new Date().getFullYear()}`;
+document.querySelector(
+	"#lastModified"
+).textContent = `Last Modification: ${document.lastModified}`;
 
 
 
