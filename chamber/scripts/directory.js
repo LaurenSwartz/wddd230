8725205b9e76e7a255c2a7dec458c75954ed06c6
LaurenSwartz <swa21010@byui.edu>
2023-03-23 23:18:00 +0200

    directory stores

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,67 +1,64 @@
-const requestURL ='https://raw.githubusercontent.com/LaurenSwartz/wdd230/main/chamber/directory.json';
+const url = "https://raw.githubusercontent.com/LaurenSwartz/wdd230/main/chamber/directory.json";
+//the following code is for grid and list, not sure if I need to put before or after url.
 const gridbutton = document.querySelector("#grid");
 const listbutton = document.querySelector("#list");
-const display = document.querySelector("article");
-
+const display = document.querySelector("#businessgrid");
 // The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
 
 gridbutton.addEventListener("click", () => {
-	// example using arrow function
-	display.classList.add("grid");
-	display.classList.remove("list");
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
 });
 
 listbutton.addEventListener("click", showList); // example using defined function
 
 function showList() {
-	display.classList.add("list");
-	display.classList.remove("grid");
+    display.classList.add("list");
+    display.classList.remove("grid");
 }
-async function getDirectoryData(url){
+//end of grid and list button
+async function getDirectoryData(url) {
     const response = await fetch(url);
     const data = await response.json();
-    //console.table(data.prophets);
+    console.log(data)
+        //console.table(data.prophets);
     displayDirectory(data.business);
 }
 
 getDirectoryData(url);
 
 const displayDirectory = (businesses) => {
-    const cards = document.querySelector('div.cards');
-
+    const cards = display; //document.querySelector('.busi'); // select the output container element
     businesses.forEach((business) => {
-        // create elements to add to the div.cards element
+        // Create elements to add to the div.cards element
         let portrait = document.createElement('img');
-        let h2 = document.createElement('section');
-        let h3 = document.createElement('h2');
-        let h4 = document.createElement('h4');
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let h3 = document.createElement('h4')
         let p = document.createElement('p');
         let a = document.createElement('a');
-        
 
-        // Build the h2 content out to show the prophets full name - finish the template string
-        h2.textContent =` ${business.companyname}`;
-        h3.textContent =`${business.phoneNumber}`;
-        p.textContent =`${business.address}`;
-        a.textContent =`${business.url}`;
-        a.setAttribute(`href`, `${business.url}`)
 
-        //Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src',business.imageurl);
-        portrait.setAttribute('alt', `${business.companyname}`);
-        portrait.setAttribute('loading','lazy');
-        portrait.setAttribute('width','340');
-        portrait.setAttribute('height','440');
+        h2.textContent = `${business.companyName}`;
+        h3.textContent = `${business.phoneNumber}`;
+        p.textContent = `${business.address}`;
+        a.textContent = `${business.url}`;
+        a.setAttribute('href', `${business.url}`)
+            // Build the image portrait by setting all the relevant attribute
+        portrait.setAttribute('src', business.imageurl);
+        portrait.setAttribute('alt', `${business.companyName}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '200');
+        portrait.setAttribute('height', '200');
 
-        //Append the section (card) with the created elements
+        // Append the section(card) with the created elements
         card.appendChild(portrait);
         card.appendChild(h2);
         card.appendChild(h3);
         card.appendChild(p);
         card.appendChild(a);
-        
-        cards.appendChild(card);
 
-        
-    });
+        cards.appendChild(card);
+    })
 }
